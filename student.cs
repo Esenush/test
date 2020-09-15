2228e0def2df86956ff28c85bb83a4abcb5a2b98
Ese <esen666@bk.ru>
2020-09-15 13:18:42 +0300

    add class student

---
+++ student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+   public class Contacts
+    {
+        public string Tel { set; get; }
+        public string address;
+    }
+    public class Student
+    {
+       public string firstName; // имя
+       public string lastName; // фамилия 
+       public byte age;
+       public Contacts contacts;
+       public int[] balls = new int[5];
+       public double average;
+
+
+    }
+}
